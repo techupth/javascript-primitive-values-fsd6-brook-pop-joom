62851181eeddffadd0364f2a6ff6b3c9104775b4
Pop <shounen.pop@gmail.com>
2024-05-09 15:39:57 +0700

    Update: ex1-8

--- ex-3.js
+++ ex-3.js
@@ -4,3 +4,7 @@ let totalFileSize = 250000;
 let uploadedSize = 30000;
 
 // Start coding here
+
+let uploadProgress = (100 / totalFileSize) * uploadedSize + "%";
+
+console.log(uploadProgress);
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let discountCoupon25Off = productPrice * 0.25;
+
+let discountPrice = productPrice - discountCoupon25Off;
+
+console.log(discountPrice);
